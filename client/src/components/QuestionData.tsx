b9981f20e21295385f84e409fad09cbdd4b7ae1d
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-10-12 11:52:10 +0700

    optimize code

--- client/src/components/QuestionData.tsx
+++ client/src/components/QuestionData.tsx
@@ -1,26 +1,25 @@
-import React, { useEffect, useState } from "react";
-import { Question } from "../interfaces/Question";
-import { Option } from "../interfaces/Option";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import {
   Checkbox,
   FormControlLabel,
   FormGroup,
   Paper,
-  colors,
 } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
+
+import { Question } from "../interfaces/Question";
+import { Option } from "../interfaces/Option";
 import { toggleCheckOptionResponse } from "../slices/responseSlice";
 import { RootState } from "../interfaces/RootState";
 
 type QuestionProps = {
-  isAllowToSwitchQuestion: boolean;
   onCheckAllowSwitchQuestion: Function;
   questionIndex: number;
   question: Question;
 };
 
 const QuestionData: React.FC<QuestionProps> = ({
-  isAllowToSwitchQuestion,
   onCheckAllowSwitchQuestion,
   questionIndex,
   question,
@@ -32,8 +31,6 @@ const QuestionData: React.FC<QuestionProps> = ({
   const handleToggleOption = (optionId: number) => {
     dispatch(toggleCheckOptionResponse({ questionId: question.id, optionId }));
   };
-
-  // const [isAllowToSwitchQuestion, setIsAllowToSwitchQuestion] = useState(false);
 
   const handleChecked = (optionId: number) => {
     const existingResponse = response.questions[questionIndex].options.find(
@@ -58,9 +55,9 @@ const QuestionData: React.FC<QuestionProps> = ({
   useEffect(() => { handleAllowToSwitchQuestion() }, [response, questionIndex]);
   return (
     <>
-      <p className="text-lg font-semibold">
-        <span>Câu {questionIndex + 1}: </span>
-        <span>{question.content}</span>
+      <p className="text-lg font-semibold text-justify">
+        <span className="underline">Câu {questionIndex + 1}:</span>
+        <span> {question.content}</span>
       </p>
       <FormGroup className="gap-3 mt-5">
         {question?.options.map((option: Option, index: number) => (
--- client/src/pages/MakeSurveyPage.tsx
+++ client/src/pages/MakeSurveyPage.tsx
@@ -1,76 +1,77 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../interfaces/RootState";
-import { useParams } from "react-router-dom";
-import { Survey } from "../interfaces/Survey";
-import { useQuestionsBySurveyId } from "../hooks";
-import { Backdrop, Button, Container } from "@mui/material";
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+
+import { Box, Button, Container } from "@mui/material";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import DoneAllIcon from "@mui/icons-material/DoneAll";
+
 import { Header, QuestionData } from "../components";
-// import useOptionsByQuestionId from "../hooks/useOptionsByQuestionId";
-import { getOptionsByQuestionId } from "../apis/QuestionApi";
-// import { saveOptionsOfQuestion } from "../slices/surveySlice";
-import { Question } from "../interfaces/Question";
-import { Option } from "../interfaces/Option";
-import { persistor } from "../app/store";
+import { RootState } from "../interfaces/RootState";
 
 const MakeSurveyPage: React.FC = () => {
-  const { surveyId } = useParams();
-  const dispatch = useDispatch();
   const currentSurvey = useSelector(
     (state: RootState) => state.survey.currentSurvey
   );
-  const [survey, setSurvey] = useState<Survey | undefined>(undefined);
-  const [response, setResponse] = useState<Response[]>([])
   const [questionIndex, setQuestionIndex] = useState<number>(0);
-  const [isAllowToSwitchQuestion, setIsAllowToSwitchQuestion] = useState<boolean>(false);
+  const [isAllowToSwitchQuestion, setIsAllowToSwitchQuestion] =
+    useState<boolean>(false);
 
-  const [isOpenConfirmationDialog, setIsOpenConfirmationDialog] = useState<boolean>(false);
-
-  console.log(isAllowToSwitchQuestion);
   return (
-    <>
-      <main className="w-full flex flex-col mb-5">
-        <Container>
-          <Header />
-          <Container maxWidth="md">
-            <section className="w-full mt-10">
-              <QuestionData
-                isAllowToSwitchQuestion={isAllowToSwitchQuestion}
-                onCheckAllowSwitchQuestion={setIsAllowToSwitchQuestion}
-                questionIndex={questionIndex}
-                question={currentSurvey?.questions[questionIndex]}
-              />
-            </section>
-            <section className={`w-full flex ${questionIndex === 0 ? 'justify-end' : 'justify-between'} mt-6`}>
-              {questionIndex > 0 && (
-                <Button
-                  onClick={() => setQuestionIndex((prev) => prev - 1)}
-                  variant="contained"
-                  disableElevation
-                  disabled={!isAllowToSwitchQuestion}
-                  startIcon={<ArrowBackIosIcon />}
-                >
-                  Câu trước
-                </Button>
-              )}
-              {questionIndex < currentSurvey?.questions?.length - 1 && (
-                <Button
-                  onClick={() => setQuestionIndex((prev) => prev + 1)}
-                  variant="contained"
-                  disableElevation
-                  disabled={!isAllowToSwitchQuestion}
-                  endIcon={<ArrowForwardIosIcon />}
-                >
-                  Câu tiếp theo
-                </Button>
-              )}
-            </section>
-          </Container>
+    <main className="w-full flex flex-col mb-5">
+      <Container>
+        <Header />
+        <Container maxWidth="md">
+          <Box component="form" className="w-full mt-10">
+            <QuestionData
+              onCheckAllowSwitchQuestion={setIsAllowToSwitchQuestion}
+              questionIndex={questionIndex}
+              question={currentSurvey?.questions[questionIndex]}
+            />
+          </Box>
+          <section
+            className={`w-full flex ${
+              questionIndex === 0 ? "justify-end" : "justify-between"
+            } mt-6`}
+          >
+            {questionIndex > 0 && (
+              <Button
+                onClick={() => setQuestionIndex((prev) => prev - 1)}
+                variant="contained"
+                disableElevation
+                disabled={!isAllowToSwitchQuestion}
+                startIcon={<ArrowBackIosIcon />}
+              >
+                Câu trước
+              </Button>
+            )}
+            {questionIndex < currentSurvey?.questions?.length - 1 && (
+              <Button
+                onClick={() => setQuestionIndex((prev) => prev + 1)}
+                variant="contained"
+                disableElevation
+                disabled={!isAllowToSwitchQuestion}
+                endIcon={<ArrowForwardIosIcon />}
+              >
+                Câu tiếp theo
+              </Button>
+            )}
+            {questionIndex === currentSurvey?.questions?.length - 1 && (
+              <Button
+                type="submit"
+                variant="contained"
+                disableElevation
+                color="success"
+                disabled={!isAllowToSwitchQuestion}
+                endIcon={<DoneAllIcon />}
+              >
+                Hoàn thành...
+              </Button>
+            )}
+          </section>
         </Container>
-      </main>
-    </>
+      </Container>
+    </main>
   );
 };
 

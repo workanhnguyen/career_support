5ab35173303d04c5cb03c7e87673aeae5d90cc23
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-10-13 22:36:57 +0700

    finish handle routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import { HollandResultDetailPage, HollandResultPage, HomePage, LandingPage, LoginPage, MakeSurveyPage, PersonalPage, RegisterPage, SurveyStarterPage } from './pages'
+import {
+  HollandResultDetailPage,
+  HollandResultPage,
+  HomePage,
+  LandingPage,
+  LoginPage,
+  MakeSurveyPage,
+  PersonalPage,
+  RegisterPage,
+  SurveyStarterPage,
+} from "./pages";
+import { useSelector } from "react-redux";
+import { RootState } from "./interfaces/RootState";
 
 const App: React.FC = () => {
+  const jwtToken = useSelector((state: RootState) => state.auth.jwtToken);
+  console.log(jwtToken === "");
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/home' element={<HomePage />} />
-        <Route path='/surveys/:surveyId' element={<SurveyStarterPage />} />
-        <Route path='/surveys/:surveyId/quiz' element={<MakeSurveyPage />} />
-        <Route path='/surveys/result' element={<HollandResultPage />} />
-        <Route path='/surveys/result/:responseId' element={<HollandResultDetailPage />} />
-        <Route path='/profile' element={<PersonalPage />} />
+        {/* Public routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Authenticated routes */}
+        {jwtToken !== "" && (
+          <>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/surveys/:surveyId" element={<SurveyStarterPage />} />
+            <Route
+              path="/surveys/:surveyId/quiz"
+              element={<MakeSurveyPage />}
+            />
+            <Route path="/surveys/result" element={<HollandResultPage />} />
+            <Route
+              path="/surveys/result/:responseId"
+              element={<HollandResultDetailPage />}
+            />
+            <Route path="/profile" element={<PersonalPage />} />
+          </>
+        )}
+
+        {/* Redirect if path is not existed */}
+        <Route path="*" element={<Navigate to='/login' />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
